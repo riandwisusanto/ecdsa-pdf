322499e2dbb0bb28ef3ea21da963e34637ae7a41
rian <>
2022-05-20 15:31:39 +0700

    add watermark in pdf

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -3,6 +3,12 @@ import ecdsa
 import PyPDF2 as pdf
 from hashlib import blake2b
 
+from reportlab.pdfgen.canvas import Canvas
+from pdfrw import PdfReader
+from pdfrw.toreportlab import makerl
+from pdfrw.buildxobj import pagexobj
+from datetime import datetime
+
 # encrypt pdf
 def encrypt(private_key, text):
     sk     = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.NIST521p, hashfunc=blake2b)
@@ -46,6 +52,40 @@ def check(text):
     
     return isSign
 
+# save sign pdf
+def saveSign(input_text):
+    input_file = "pdf/"+input_text+".pdf"
+    output_file = "pdf-sign/"+input_text+"_sign.pdf"
+
+    # Get pages
+    reader = PdfReader(input_file)
+    pages = [pagexobj(p) for p in reader.pages]
+
+
+    # Compose new pdf
+    canvas = Canvas(output_file)
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    for page_num, page in enumerate(pages, start=1):
+
+        # Add page
+        canvas.setPageSize((page.BBox[2], page.BBox[3]))
+        canvas.doForm(makerl(canvas, page))
+
+        # Draw footer
+        footer_text = "Telah ditanda tangani "+dt_string
+        x = 160
+        canvas.saveState()
+        canvas.setFont('Times-Roman', 8)
+        canvas.drawString(page.BBox[2]-x, 20, footer_text)
+        canvas.restoreState()
+
+        canvas.showPage()
+
+    canvas.save()
+
 if __name__ == '__main__':
     print("--- ENCRYPT DECRYPT PDF ---")
     print("1. Sign PDF")
@@ -69,16 +109,17 @@ if __name__ == '__main__':
             print("private key            :", private_key)
             print("public  key            :", public_key)
             print("signature              :", signature)
-            
-            print("waktu proses sign      : %s second" % (time.time() - sign_time))
 
             rl = readFile()
             rl.append(private_key+"_"+signature)
-
             with open('signature.txt', 'w') as f:
                 for line in rl:
                     f.write(line)
                     f.write("\n")
+            
+            saveSign(input_pdf)
+
+            print("waktu proses sign      : %s second" % (time.time() - sign_time))
     else:
         input_pdf    = input("masukkan nama file PDF : ")
         input_stream = pdf.PdfFileReader(open("pdf/"+ input_pdf + ".pdf", "rb"))
