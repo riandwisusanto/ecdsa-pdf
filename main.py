7ffd497653b656f62345ecef4733bfe893799abf
riandwisusanto <riandwisusanto03@gmail.com>
2022-06-15 23:28:20 +0700

    revision again

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 import time
 import ecdsa
 import PyPDF2 as pdf
-from hashlib import blake2b
+import hashlib
+from difflib import SequenceMatcher
 
 from reportlab.pdfgen.canvas import Canvas
 from pdfrw import PdfReader
@@ -12,7 +14,7 @@ from datetime import datetime
 
 # encrypt pdf
 def encrypt(private_key, text):
-    sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.NIST521p, hashfunc=blake2b)
+    sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.NIST521p, hashfunc=hashlib.blake2b)
     enkrip = sk.sign(text.encode('utf-8'))
 
     return enkrip.hex()
@@ -21,8 +23,8 @@ def encrypt(private_key, text):
 # decrypt pdf
 def decrypt(public_key, signature, text):
     try:
-        vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key), curve=ecdsa.NIST521p, hashfunc=blake2b)
-        return vk.verify(bytes.fromhex(signature), text.encode('utf-8'), blake2b)
+        vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key), curve=ecdsa.NIST521p, hashfunc=hashlib.blake2b)
+        return vk.verify(bytes.fromhex(signature), text.encode('utf-8'), hashlib.blake2b)
     except:
         return False
 
@@ -45,11 +47,11 @@ def check(text):
             priv = split[0]
             sig = split[1]
 
-            vk = ecdsa.SigningKey.from_string(bytes.fromhex(priv), curve=ecdsa.NIST521p, hashfunc=blake2b).verifying_key
+            vk = ecdsa.SigningKey.from_string(bytes.fromhex(priv), curve=ecdsa.NIST521p, hashfunc=hashlib.blake2b).verifying_key
             vk.precompute()
 
             try:
-                isSign = vk.verify(bytes.fromhex(sig), text.encode('utf-8'), blake2b)
+                isSign = vk.verify(bytes.fromhex(sig), text.encode('utf-8'), hashlib.blake2b)
                 break
             except ecdsa.BadSignatureError:
                 isSign = False
@@ -59,36 +61,66 @@ def check(text):
 
 # save sign pdf
 def saveSign(input_text):
-    input_file = "pdf/" + input_text + ".pdf"
+    input_file  = "pdf/" + input_text + ".pdf"
     output_file = "pdf-sign/" + input_text + "_sign.pdf"
+    output_temp = "pdf-temp/" + input_text + "_temp.pdf"
 
     # Get pages
     reader = PdfReader(input_file)
     pages = [pagexobj(p) for p in reader.pages]
 
     # Compose new pdf
-    canvas = Canvas(output_file)
+    canvas_file = Canvas(output_file)
+    canvas_temp = Canvas(output_temp)
 
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
     for page_num, page in enumerate(pages, start=1):
         # Add page
-        canvas.setPageSize((page.BBox[2], page.BBox[3]))
-        canvas.doForm(makerl(canvas, page))
+        canvas_file.setPageSize((page.BBox[2], page.BBox[3]))
+        canvas_file.doForm(makerl(canvas_file, page))
+
+        canvas_temp.setPageSize((page.BBox[2], page.BBox[3]))
+        canvas_temp.doForm(makerl(canvas_temp, page))
 
         # Draw footer
         footer_text = "Telah ditanda tangani " + dt_string
         x = 160
-        canvas.saveState()
-        canvas.setFont('Times-Roman', 8)
-        canvas.drawString(page.BBox[2] - x, 20, footer_text)
-        canvas.restoreState()
+        canvas_file.saveState()
+        canvas_file.setFont('Times-Roman', 8)
+        canvas_file.drawString(page.BBox[2] - x, 20, footer_text)
+        canvas_file.restoreState()
+        canvas_temp.saveState()
+        canvas_temp.setFont('Times-Roman', 8)
+        canvas_temp.drawString(page.BBox[2] - x, 20, footer_text)
+        canvas_temp.restoreState()
 
-        canvas.showPage()
 
-    canvas.save()
+        canvas_file.showPage()
+        canvas_temp.showPage()
 
+    canvas_file.save()
+    canvas_temp.save()
+
+
+def diff_file(fileName1, fileName2):
+    h1 = hashlib.sha1()
+    h2 = hashlib.sha1()
+  
+    with open(fileName1, "rb") as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h1.update(chunk)
+              
+    with open(fileName2, "rb") as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h2.update(chunk)
+  
+        return h1.hexdigest(), h2.hexdigest()
 
 if __name__ == '__main__':
     print("--- ENKRIPSI DAN DEKRIPSI DOKUMEN PDF ---")
@@ -101,7 +133,7 @@ if __name__ == '__main__':
         input_stream = pdf.PdfFileReader(open("pdf/" + input_pdf + ".pdf", "rb"))
         text_stream = input_stream.getPage(0).extractText()
 
-        sk = ecdsa.SigningKey.generate(curve=ecdsa.NIST521p, hashfunc=blake2b)
+        sk = ecdsa.SigningKey.generate(curve=ecdsa.NIST521p, hashfunc=hashlib.blake2b)
         private_key = sk.to_string().hex()
         public_key = sk.verifying_key.to_string().hex()
         sign_time = time.time()
@@ -115,7 +147,7 @@ if __name__ == '__main__':
             print("Signature              :", signature)
 
             rl = readFile()
-            rl.append(signature + " -_- " + public_key)
+            rl.append(private_key + "_" + signature)
             with open('signature.txt', 'w') as f:
                 for line in rl:
                     f.write(line)
@@ -130,12 +162,19 @@ if __name__ == '__main__':
         text_stream = input_stream.getPage(0).extractText()
 
         if check(text_stream):
+            input_stream = pdf.PdfFileReader(open("pdf/" + input_pdf + ".pdf", "rb"))
+            text_stream  = input_stream.getPage(0).extractText()
             input_sig = input("Masukkan Signature     : ")
             signature = input_sig
             input_pub = input("Masukkan Public Key    : ")
 
             verify_time = time.time()
-            print("Signature              :", decrypt(input_pub, signature, text_stream))
+
+            pdf1, pdf2 = diff_file("pdf-sign/" + input_pdf + "_sign.pdf", "pdf-temp/" + input_pdf + "_temp.pdf")
+            if pdf1 != pdf2:
+                print("Signature              : False")
+            else:
+                print("Signature              :", decrypt(input_pub, signature, text_stream))
             print("Waktu proses verify    : %s second" % (time.time() - verify_time))
         else:
             print("PDF belum di tanda tangani")
